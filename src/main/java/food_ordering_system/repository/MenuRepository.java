24a6d9a54687d879c3fecab5436030ebfa545ae7
renne99 <asisiphonosasa@gmail.com>
2026-06-29 15:20:24 +0200

    feat(menu): MenuRepository

---
+++ src/main/java/food_ordering_system/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package food_ordering_system.repository;
+
+import food_ordering_system.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
